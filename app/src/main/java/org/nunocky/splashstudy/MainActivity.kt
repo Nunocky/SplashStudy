1381c87517773d8297941c19f8ca667b46741fd8
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2022-05-05 14:01:09 +0900

    Initial commit

---
+++ app/src/main/java/org/nunocky/splashstudy/MainActivity.kt
@@ -0,0 +1,51 @@
+package org.nunocky.splashstudy
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    var isReady = false
+
+    init {
+        viewModelScope.launch {
+            delay(1000)
+            isReady = true
+        }
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+        // Set up an OnPreDrawListener to the root view.
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (viewModel.isReady) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
+    }
+}
